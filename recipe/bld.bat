5d66ca8079d51d5bb76ba1eddf3c1235814387bc
Nicholas Bollweg <nbollweg@continuum.io>
2016-06-29 09:08:04 -0400

    build on windows with cmake

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,22 @@
-set NOPAUSE=true
+mkdir build
 
-cd builds\msvc\build
+cd build
 
-buildall.bat
-IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+cmake ^
+  -G "%CMAKE_GENERATOR%" ^
+  -D BUILD_FOR_DISTRIBUTION="yes" ^
+  -D BUILD_SHARED_LIBS="yes" ^
+  -D BUILD_STATIC_LIBS="yes" ^
+  -D CMAKE_BUILD_TYPE="Release" ^
+  -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+  -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+  -D PYTHON_EXECUTABLE="%PYTHON%" ^
+  -D ZMQ_BUILD_TESTS="no" ^
+  ..
+if errorlevel 1 exit 1
+
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
