a6a1be31f157fa11bf89e4bf1200fc0d4d5b99f8
Nicholas Bollweg <nbollweg@continuum.io>
2016-06-29 10:05:28 -0400

    more win attempts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,16 +2,15 @@ mkdir build
 
 cd build
 
+set CMAKE_CONFIG="Release"
+
 cmake ^
-  -G "%CMAKE_GENERATOR%" ^
-  -D BUILD_FOR_DISTRIBUTION="yes" ^
-  -D BUILD_SHARED_LIBS="yes" ^
-  -D BUILD_STATIC_LIBS="yes" ^
-  -D CMAKE_BUILD_TYPE="Release" ^
   -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
   -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
   -D PYTHON_EXECUTABLE="%PYTHON%" ^
+  -D PYTHON_INCLUDE_DIR="%PREFIX%/include" ^
   -D ZMQ_BUILD_TESTS="no" ^
+  -G "%CMAKE_GENERATOR%" ^
   ..
 if errorlevel 1 exit 1
 
