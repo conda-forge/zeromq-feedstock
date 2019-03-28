e77b6be72576b6ac0821e9f4d7a60400908b0aee
E. G. Patrick Bos <egpbos@gmail.com>
2019-03-28 08:52:02 +0100

    try to fix ms dos copy syntax error

--- recipe/install_perf.bat
+++ recipe/install_perf.bat
@@ -1,4 +1,8 @@
 cd build
 
-copy /y .\bin\*_lat.exe .\bin\*_thr.exe .\bin\benchmark_radix_tree.exe /b %LIBRARY_BIN%\
+copy /y .\bin\*_lat.exe /b %LIBRARY_BIN%\
+if errorlevel 1 exit 1
+copy /y .\bin\*_thr.exe /b %LIBRARY_BIN%\
+if errorlevel 1 exit 1
+copy /y .\bin\benchmark_radix_tree.exe /b %LIBRARY_BIN%\
 if errorlevel 1 exit 1
